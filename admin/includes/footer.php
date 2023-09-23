4ab6798a5e30fd941cbd375c41817b852ec4596d
Youssef Ashraf <Kingjo777ashraf@gmail.com>
2023-09-23 10:00:10 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,11 @@
+<section class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; <?php echo date('Y'); ?> Online Library Management System |<a target="_blank"> Designed
+                    by : Youssef Ashraf</a>
+            </div>
+
+        </div>
+    </div>
+</section>
